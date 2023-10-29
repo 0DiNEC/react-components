1f8c6382c10f315992fdbc07b548051469d1ae5a
Andrey <as.odinson@gmail.com>
2023-10-29 22:01:20 +0300

    feat: add search components and test husky

---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Search = () => {
+  return (
+    <div>
+      <input type="text" 
+      className="search-bar" 
+      placeholder="write text" />
+      <button type="submit">
+        
+        
+        search</button>
+    </div>
+  );
+};
+
+export default Search;
