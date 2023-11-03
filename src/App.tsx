e4b17df3f3ca0c56a59e61db4ef8c3fac053c44d
Andrey <as.odinson@gmail.com>
2023-11-03 20:54:24 +0300

    feat: test-husky

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,13 @@ import { useState } from 'react';
 
 import './App.css';
 
-const App = () => {
-  return <div>APP</div>;
+function App() {
+  return <div>APP
+
+
+
+    
+  </div>;
 }
 
 export default App;
-
